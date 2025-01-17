a7f2ae99f312e6499acffbbe15d8b5aa8a30ee9a
Murtuza Rangwala <murtuza.rangwala@1Rintern.local>
2025-01-17 15:27:28 +0530

    user details fetch code

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("Hello JavaScript !")
+let firstName;
+let lastName;
+
+function showFullName(firstName = "Murtuza", lastName = "Rangwala") {
+    console.log(`${firstName} ${lastName}`);
+}
+
+showFullName();
+showFullName("Murtuza",  "Master");
